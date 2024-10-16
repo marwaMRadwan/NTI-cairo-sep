3ecd3ad78bb016ec8b1d4cf4876334cbfe2ba661
marwaMRadwan <44086877+marwaMRadwan@users.noreply.github.com>
2024-10-16 20:53:28 +0300

    s15

---
+++ session15/js/custom.js
@@ -0,0 +1,127 @@
+// functions
+// function hello(){
+//     console.log("hello")
+// }
+
+// function helloWithName(email, userName = "Omar"){
+//     console.log(`hello ${userName}`)
+// }
+
+// hello()
+
+// let name = "Marwa"
+// helloWithName("marwa@techsexperts.com")
+
+// function add(x,y){
+//     return x + y
+//     console.log("test")
+// }
+
+// let result = add(5,3)
+
+
+// let degree = 50
+
+// function getGrade(){
+//     let x = 5
+//     if(degree>=50) console.log("pass")
+//     else console.log("fail")
+//     return x
+// }
+// getGrade()
+// console.log(x)
+
+
+//get data
+// function getDataFromUser(message){
+//     return prompt(message)
+// }
+
+// function claculateBMI(h, w, flag=true){
+//     if(h>3000) return console.log("there is an error")
+//     if(flag) h=h/100
+//     return w/(h**2)
+// }
+// function displayBMI(){
+//     let w = +getDataFromUser("enter p w")
+//     let h = +getDataFromUser("enter p h")
+//     let bmi = claculateBMI(h,w)
+//     console.log(`h1 is ${ h} and w is ${w} p1 bmi is ${bmi}`)
+//     return bmi
+// }
+
+// let h1 = +getDataFromUser("please enter p1 h")
+
+
+// let deg = +getDataFromUser("please enter your degree")
+// function getGrade(deg){
+//     let grade = "F"
+//     if(deg>=90) grade = "A"
+//     else if(deg>=80) grade = "B"
+//     else if(deg>=70) grade = "C"
+//     else if(deg>=60) grade = "D"
+//     // else  grade = "F"
+//     return grade
+// }
+
+
+// let x = console.log(5)
+// console.log(x)
+
+// function getDataFromUser(msg){
+//     return prompt(msg)
+// }
+
+// function claculateBMI(h, w){
+//     return w/(h**2)
+// }
+// function displayBMI(userName){
+//     let h = getDataFromUser(`enter ${userName} h`)
+//     let w = getDataFromUser(`enter ${userName} w
+//         `)
+//     let bmi = claculateBMI(h,w)
+//     return bmi
+// }
+
+// let myBmi = displayBMI("Marwa")
+// alert(myBmi)
+
+
+// function getDataFromUser (){
+//     return 5
+// }
+// getDataFromUser()
+// const getDataFromUser = function () {
+//     console.log(this)
+//     return 5
+// }
+
+// const getDataFromUser1 = () => {
+//     console.log("******")
+//     console.log(this)
+//     return 5
+// }
+
+// getDataFromUser()
+// getDataFromUser1()
+
+
+// const getDataFromUser = (msg) => +prompt(msg)
+// const calculateBMI = (h,w) => w/(h**2)
+
+// console.log(getDataFromUser())
+
+
+
+// throw new Error("dhghd")
+let data = ["ahmed", 23, false, {}, [], 5.5, function(){}]
+console.log(data)
+let esraa = {
+    name: "esraa",
+    age: 20, 
+    email: "esraa@nti.com"
+}
+
+
+
+
